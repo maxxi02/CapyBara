740b00a49a78381371cbf1287b23ba06cc8fd1b0
maxxi <129664117+maxxi02@users.noreply.github.com>
2023-08-15 16:25:27 +0800

    update4444

--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -1,48 +1,49 @@
-import React from 'react'
-import styles from './page.module.css'
-import Link from 'next/link'
-import Image from 'next/image'
+"use client"
 
-
-
+import React, { useState, useEffect } from 'react';
+import styles from './page.module.css';
+import Link from 'next/link';
+import Image from 'next/image';
 
 async function getData() {
-  const res = await fetch('http://localhost:3000/api/posts' , {cache: 'no-store'}, )
+  const res = await fetch('http://localhost:3000/api/posts', { cache: 'no-store' });
 
- 
   if (!res.ok) {
-
-    throw new Error('Failed to fetch data')
+    throw new Error('Failed to fetch data');
   }
- 
-  return res.json()
+
+  return res.json();
 }
 
+export default function Blog() {
+  const [data, setData] = useState([]);
 
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const fetchedData = await getData();
+        setData(fetchedData);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    }
 
-
-export default async function Blog() {
-
-
-  const data = await getData();
-
-
+    fetchData();
+  }, []);
 
   return (
     <div className={styles.mainContainer}>
-      {data.map((item)=>
-      {
-
-       return (<Link key={item.id} href={`/blog/${item._id}`} className={styles.links}>
-        <div className={styles.imgContainer}>
-          <Image src={item.img} alt='' height={250} width={400} className={styles.imgContainer} />
-        </div>
-        <div  className={styles.content}>
-          <h1 className={styles.title}>{item.title}</h1>
-          <p  className={styles.desc}>{item.desc}</p>
-        </div>
-      </Link>)
-      })}
+      {data.map((item) => (
+        <Link key={item._id} href={`/blog/${item._id}`} className={styles.links}>
+          <div className={styles.imgContainer}>
+            <Image src={item.img} alt='' height={250} width={400} className={styles.imgContainer} />
+          </div>
+          <div className={styles.content}>
+            <h1 className={styles.title}>{item.title}</h1>
+            <p className={styles.desc}>{item.desc}</p>
+          </div>
+        </Link>
+      ))}
     </div>
-  )
+  );
 }
